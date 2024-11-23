b9f39c7363fb8f5585053307ffede4cec3ea45a8
choejonghyeon <choijonghyen@gmail.com>
2024-11-23 20:43:04 +0900

    refactor: @EnableWebMvc 추가

--- src/main/java/teamproject/aipro/AiProApplication.java
+++ src/main/java/teamproject/aipro/AiProApplication.java
@@ -2,7 +2,9 @@ package teamproject.aipro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class AiProApplication {
 
