fd4f0a500bbaae4847f11cf85335e06822c08967
choejonghyeon <choijonghyen@gmail.com>
2024-11-24 12:24:47 +0900

    refactor: test endpoint 변경

--- src/main/java/teamproject/aipro/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/teamproject/aipro/config/jwt/JwtAuthenticationFilter.java
@@ -36,7 +36,7 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
 		throws IOException, ServletException {
 		String path = request.getRequestURI();
 
-		if (path.startsWith("/api/member") || path.startsWith("/api/health")) {
+		if (path.startsWith("/api/member") || path.startsWith("/api/test")) {
 			chain.doFilter(request, response);
 			return;
 		}
