e7614aee7e969714e58ea1cc100368fee6819886
choejonghyeon <choijonghyen@gmail.com>
2024-11-24 12:25:06 +0900

    feat: test controller 추가

---
+++ src/main/java/teamproject/aipro/domain/test/controller/TestController.java
@@ -0,0 +1,29 @@
+package teamproject.aipro.domain.test.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> check() {
+        return ResponseEntity.ok("OK");
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+//            .body("BAD_REQUEST");
+    }
+
+    @GetMapping("/ver")
+    public int getVersion() {
+        return 2;
+    }
+
+    @GetMapping("/rollback")
+    public ResponseEntity<String> rollback() {
+        return ResponseEntity.ok("OK");
+    }
+
+}
