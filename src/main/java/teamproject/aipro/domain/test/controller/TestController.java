56039a676a2426f5432e87631b236a33e769955f
choejonghyeon <choijonghyen@gmail.com>
2024-12-04 17:33:06 +0900

    refactor: badrequest로 변경

--- src/main/java/teamproject/aipro/domain/test/controller/TestController.java
+++ src/main/java/teamproject/aipro/domain/test/controller/TestController.java
@@ -1,5 +1,6 @@
 package teamproject.aipro.domain.test.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
